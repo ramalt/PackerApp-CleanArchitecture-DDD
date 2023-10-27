b36aceb958eab801c35f5a23cd7a4cd5639f12de
ramalt <ramazanalltuntepe@gmail.com>
2023-10-27 21:32:54 +0300

    - Migrations created for ReadDbContext

---
+++ src/PackerApp.Infrastructure/EFCore/Migrations/20231027183228_InitialCreateForReadContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PackerApp.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateForReadContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "packings");
+
+            migrationBuilder.CreateTable(
+                name: "PackingLists",
+                schema: "packings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Localization = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackingItems",
+                schema: "packings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<long>(type: "bigint", nullable: false),
+                    IsPacked = table.Column<bool>(type: "boolean", nullable: false),
+                    PackingListId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PackingItems_PackingLists_PackingListId",
+                        column: x => x.PackingListId,
+                        principalSchema: "packings",
+                        principalTable: "PackingLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackingItems_PackingListId",
+                schema: "packings",
+                table: "PackingItems",
+                column: "PackingListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PackingItems",
+                schema: "packings");
+
+            migrationBuilder.DropTable(
+                name: "PackingLists",
+                schema: "packings");
+        }
+    }
+}
